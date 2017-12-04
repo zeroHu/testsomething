214f064e69b904288635f23763a92beec4dd12ad
jianxia <jianxia@xfz.cn>
2017-12-04 14:10:29 +0800

    add some

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 const Koa = require('koa');
-const Krouter = require('koa-route');
+const router = require('koa-router')();
 const sha1 = require('sha1');
 const wechat = require('./wechat/g');
 const config = require('./config');
@@ -9,14 +9,14 @@ const weixin = require('./weixin');
 let app = new Koa();
 
 // 定义路由
-const auth = require('./router').auth;
+const RouteFn = require('./router');
 
 // 引用中间件
 app.use(wechat(config.wechat,weixin.reply));
 
 console.log('-----start router ------');
-// 路由
-app.use(Krouter.get('/auth', auth));
+
+router.post('/auth',RouteFn.auth);
 
 app.listen(3006);
 console.log('listening 3006');
